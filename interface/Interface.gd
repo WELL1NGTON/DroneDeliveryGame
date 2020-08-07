9abccc44f5df7236149ee667ad1ba2774a421709
Wellington <wellingtonmassola@outlook.com>
2020-08-07 16:47:46 -0300

    Change Interface and KinematicBody Player

---
+++ interface/Interface.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rect_size = get_tree().get_root().get_viewport().size * get_parent().zoom
+	rect_position = rect_size*-0.5
+
+	pass
